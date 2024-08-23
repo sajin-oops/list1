79e9a3d81a265defc469ccd7f9ec6ffe49ac64a4
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-23 20:15:18 +0530

    Add files via upload

---
+++ set1.py
@@ -0,0 +1,15 @@
+mylist = ["sajin","lijin","aaron"]
+print(mylist)
+#O/P - ['sajin', 'lijin', 'aaron']
+mylist.append("nijas")
+print(mylist)
+#O/P - ['sajin', 'lijin', 'aaron', 'nijas']
+x = mylist.count("sajin")
+print(x)
+#O/P - 1
+print(len(mylist))
+
+
+# - A list can contain different data types:
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
